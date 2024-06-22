85c0222d91d0921d54aed7d2d5059fced0d3092f
Nimesh Jayaweera <105283769+nim3xh@users.noreply.github.com>
2024-06-22 23:13:43 +0530

    person.cpp

---
+++ Person.cpp
@@ -0,0 +1,34 @@
+#include "Person.h"
+
+Person::Person() {
+
+}
+
+
+Person::Person(const std::string& name, const std::string& address, const std::string& email)
+        : name(name), address(address), email(email) {
+}
+
+std::string Person::getName() const {
+    return name;
+}
+
+std::string Person::getAddress() const {
+    return address;
+}
+
+std::string Person::getEmail() const {
+    return email;
+}
+
+void Person::setName(const std::string& newName) {
+    name = newName;
+}
+
+void Person::setAddress(const std::string& newAddress) {
+    address = newAddress;
+}
+
+void Person::setEmail(const std::string& newEmail) {
+    email = newEmail;
+}
