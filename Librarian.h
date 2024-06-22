911f95c522cd0bd386b202262a55c210dfdd34ff
Nimesh Jayaweera <105283769+nim3xh@users.noreply.github.com>
2024-06-22 23:12:58 +0530

    librarian.h

---
+++ Librarian.h
@@ -0,0 +1,33 @@
+#ifndef LIBRARIAN_H
+#define LIBRARIAN_H
+
+#include "Person.h"
+#include "Member.h"
+#include "Book.h"
+#include "Date.h"
+#include <vector>
+
+class Librarian : public Person {
+private:
+    int salary;
+    int staffID;
+    std::vector<Member> members;
+
+
+public:
+    std::vector<Book> books;
+    Librarian(int staffID, const std::string& name, const std::string& address, const std::string& email, int salary);
+
+    void addMember();
+    void issueBook(int memberID, int bookID);
+    void returnBook(int memberID, int bookID);
+    void displayBorrowedBooks(int memberID);
+    void calcFine(int memberID);
+
+    int getStaffID() const;
+    int getSalary() const;
+    void setStaffID(int newStaffID);
+    void setSalary(int newSalary);
+};
+
+#endif
